d645d29de2bf2cb029e1141cb598dc736858f7a7
Alza Arh <alza.arhane97@gmail.com>
2021-04-10 13:25:03 +0430

    add discount_codes table

---
+++ database/migrations/2021_04_10_130211_create_discount_codes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->integer('max')->nullable();
+            $table->integer('min')->nullable();
+            $table->integer('percent')->nullable();
+            $table->integer('value')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('used_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount_codes');
+    }
+}
