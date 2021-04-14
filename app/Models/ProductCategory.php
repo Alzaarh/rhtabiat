8ed0ec33808b3eaca2dbb792d91047a4eaf70564
Alza Arh <alza.arhane97@gmail.com>
2021-04-14 23:32:20 +0430

    remove hasImage trait from productCategory model

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-use App\Traits\HasImage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
     use HasFactory;
-//    use HasImage;
 
     protected $guarded = [];
 
