e02bc3d92f2382f28ae2d66199eae19eacda5302
Alza Arh <alza.arhane97@gmail.com>
2021-04-14 23:31:39 +0430

    remove hasImage trait from banner model

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -2,18 +2,12 @@
 
 namespace App\Models;
 
-use App\Traits\HasImage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
     use HasFactory;
-    use HasImage;
-
-    public $timestamps = false;
-
-    protected $guarded = [];
 
     const LOCATIONS = [
         'hero' => 1,
@@ -23,6 +17,8 @@ class Banner extends Model
         'home_mob_slider' => 5,
         'home_mob_small' => 6,
     ];
+    public $timestamps = false;
+    protected $guarded = [];
 
     public function getLocationAttribute($location)
     {
