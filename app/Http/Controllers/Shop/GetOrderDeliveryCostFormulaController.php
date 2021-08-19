6138c824a9222e8d8edf7cc1c82c70abe1d73eac
Alza Arh <alza.arhane97@gmail.com>
2021-08-19 12:01:16 +0430

    Update delivery cost formula

--- app/Http/Controllers/Shop/GetOrderDeliveryCostFormulaController.php
+++ app/Http/Controllers/Shop/GetOrderDeliveryCostFormulaController.php
@@ -5,29 +5,22 @@ namespace App\Http\Controllers\Shop;
 use App\Actions\TranslateProductUnitsAction;
 use App\Models\Order;
 use App\Models\Product;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
 class GetOrderDeliveryCostFormulaController
 {
-    public function __invoke(Request $request, TranslateProductUnitsAction $action)
+    public function __invoke(TranslateProductUnitsAction $action)
     {
-        $units = $action->execute();
+        $kilogramUnit = $action->execute()->search(Product::UNITS['kilogram']);
+        $withingProvince = Order::WITHIN_PROVINCE;
+        $functionBody = "if(u==='$kilogramUnit'){";
+        $functionBody .= "if(p===$withingProvince){return(((w+0.15)*9800)+2500)*1.1}";
+        $functionBody .= "else{return(((w+0.15)*14000)+2500)*1.1}}";
+        $functionBody .= "else{return 20000}";
 
-        $request->validate(['unit' => ['required', Rule::in($units->keys())]]);
-
-        if (Product::UNITS['kilogram'] === $units->get($request->query('unit'))) {
-            $withingProvince = Order::WITHIN_PROVINCE;
-            $function = [
-                'arguments' => 'p,w',
-                'body' => "if (p === $withingProvince) {return (((w+0.15)*9800)+2500)*1.1} else {return (((w+0.15)*14000)+2500)*1.1}"
-            ];
-        } else {
-            $function = [
-                'arguments' => '',
-                'body' => 'return 20000',
-            ];
-        }
+        $function = [
+            'arguments' => 'p,w,u',
+            'body' => $functionBody,
+        ];
 
         return response()->json(['data' => $function]);
     }
