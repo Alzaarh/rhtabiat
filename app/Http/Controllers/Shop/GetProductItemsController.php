9c9296bf8bbbf92acf3131849a00c3e548c9198b
Alza Arh <alza.arhane97@gmail.com>
2021-08-19 13:01:39 +0430

    Refactor get product items

--- app/Http/Controllers/Shop/GetProductItemsController.php
+++ app/Http/Controllers/Shop/GetProductItemsController.php
@@ -5,21 +5,19 @@ namespace App\Http\Controllers\Shop;
 use App\Http\Resources\ProductItemResource;
 use App\Models\ProductItem;
 use DB;
+use Illuminate\Http\Request;
 
 class GetProductItemsController
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        request()->validate([
-            'products' => 'required|array',
-            'products.*' => 'required|exists:product_items,id',
-        ]);
+        $request->validate(['products' => 'required|array']);
 
-        $idArray = implode(',', request()->products);
+        $ids = implode(',', $request->query('products'));
+
         return ProductItemResource::collection(
-            ProductItem::with('product')
-                ->whereIn('id', request()->products)
-                ->orderByRaw(DB::raw("FIELD(id, $idArray)"))
+            ProductItem::with('product.image')->whereIn('id', request()->products)
+                ->orderByRaw(DB::raw("FIELD(id, $ids)"))
                 ->get()
         );
     }
